31b160b9ccfec765c0237db1f9545e9c00f3486e
Kosetsue <Kosetsue@gmail.com>
2023-07-28 16:15:24 +0800

    Refactored behavior for generic views

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,4 +3,9 @@ from restapi import views
 
 urlpatterns = [
     path("expenses/", views.ExpenseListCreate.as_view(), name="expense-list-create"),
+    path(
+        "expenses/<pk>",
+        views.ExpenseRetrieveDelete.as_view(),
+        name="expense-retrieve-delete",
+    ),
 ]
